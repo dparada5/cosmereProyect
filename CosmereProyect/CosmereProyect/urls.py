b45bcd270f9645ce1c2dd926cd1481d2f9e2f564
root <root@LAPTOP-K1582UN0>
2024-09-17 18:30:09 +0200

    permissions fixed

--- CosmereProyect/CosmereProyect/urls.py
+++ CosmereProyect/CosmereProyect/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from django.urls import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registration/', views.register, name='registration'),
 ]
--- CosmereProyect/users/views.py
+++ CosmereProyect/users/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test #admin
+from django.contrib.admin.views.decorators import staff_member_required #staff
+
+def	registration(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+	else:
+		form = UserCreationForm()
+	return render(request, 'registration.html', {'form': form})
+
+#ejemplos de niveles de usuario
+def	is_admin(user):
+	return user.is_superuser
+
+@user_passes_test(is_admin)
+def	admin_view(request):
+	return render(request, 'admin.html')
+
+@staff_member_required
+def	staff_view(request):
+	return render(request, 'staff.html')
